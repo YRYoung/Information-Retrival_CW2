2d0775a535c89c43cd9210b9c70a4c1d79afad19
Mechrevo <mechrevo@example.com>
2023-03-04 12:47:40 +0000

    Change the name of task5.py

---
+++ logistic_regression.py
@@ -0,0 +1,20 @@
+"""
+Logistic Regression (LR)(25 marks)
+
+Represent passages and query based on a word embedding method,
+(such as Word2Vec, GloVe, FastText, or ELMo).
+
+Compute query (/passage) embeddings by averaging embeddings of all the words in that query (/passage).
+
+With these query and passage embeddings as input, implement a logistic regression model
+to assess relevance of a passage to a given query.
+
+Describe how you perform input processing & representation or features used.
+
+Using the metrics you have implemented in the previous part,
+report the performance of your model based on the validation data.
+
+Analyze the effect of the learning rate on the model training loss.
+
+(All implementations for logistic regression algorithm must be your own for this part.)
+"""
