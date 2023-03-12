9d384fa5ae91f16f70907a4e1517c7e9a2b87dfd
Mechrevo <mechrevo@example.com>
2023-03-12 06:39:15 +0000

    add initial funcs

--- LambdaMART.py
+++ LambdaMART.py
@@ -19,3 +19,41 @@ Report:
         2. the representation/features used as input
 
 """
+
+import xgboost as xgb
+
+### Splitting training set ###
+# x_train, x_valid, y_train, y_valid = train_test_split(train, target,
+#                                                       random_state=42,
+#                                                           test_size=0.3)
+
+def custom_eval(preds, dtrain):
+    labels = dtrain.get_label().astype(np.int)
+    preds = (preds >= 0.3).astype(np.int)
+    return [('f1_score', f1_score(labels, preds))]
+
+### XGBoost compatible data ###
+dtrain = xgb.DMatrix(x_train,y_train)
+dvalid = xgb.DMatrix(x_valid, label = y_valid)
+
+### defining parameters ###
+params = {
+          'colsample': 0.9,
+          'colsample_bytree': 0.5,
+          'eta': 0.1,
+          'max_depth': 8,
+          'min_child_weight': 6,
+          'objective': 'binary:logistic',
+          'subsample': 0.9
+          }
+
+### Training the model ###
+xgb_model = xgb.train(
+                      params,
+                      dtrain,
+                      feval= custom_eval,
+                      num_boost_round= 1000,
+                      maximize=True,
+                      evals=[(dvalid, "Validation")],
+                      early_stopping_rounds=30
+                      )
