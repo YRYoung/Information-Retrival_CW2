46fcf845e9f4d10c8f1779e73bda1af09da1dd19
Mechrevo <mechrevo@example.com>
2023-03-12 06:37:46 +0000

    add add_q_idx()

--- prepocessing.py
+++ prepocessing.py
@@ -10,11 +10,15 @@ sample_tsv = f'{data_path}/part2/sample_data.tsv'
 
 
 def add_q_idx():
-    df = read_csv(csv_path=train_tsv)
-    df1 = df.sort_values(by=['qid', 'pid'])
-    _, b = np.unique(df1.qid.values, return_counts=True)
-    df1['p_idx'] = np.repeat(np.arange(len(b)), b)
+    df = read_csv(train_tsv)
+    df1 = df.sort_values(by=['qid', 'relevancy'], ascending=[True, False])
+    _, count_repeats = np.unique(df1.qid.values, return_counts=True)
 
-    df1.to_parquet(f'{output_path}/train_data_raw.parquet.gzip', compression='gzip')
+    df1['q_idx'] = np.repeat(np.arange(len(count_repeats)), count_repeats)
+
+    df1['p_idx'] = np.hstack([np.arange(bb) for bb in count_repeats])
+    df1.to_parquet(out_file, compression='gzip')
+
+
 def clean():
 
