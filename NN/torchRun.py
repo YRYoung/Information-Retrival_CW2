5839d9cfc02cdf8e539131ff4f3aa8091ccebab9
Mechrevo <mechrevo@example.com>
2023-03-22 20:18:23 +0000

    extract evaluation n training to function

--- NN/torchRun.py
+++ NN/torchRun.py
@@ -9,9 +9,10 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from CustomDataset import ValidationDataset
+from CustomDataset import ValidationDataset, CustomDataset
 from NN.torchModelv2 import PytorchCNN, MultiMarginRankingLoss
-from utils import data_path, train_raw_df, map_location
+from eval import eval_per_query
+from utils import data_path, train_raw_df, map_location, val_raw_df
 
 models_save_dir = f'{data_path}/models'
 log_root_dir = f'{data_path}/logs'
@@ -84,7 +85,9 @@ def train_epoch_2(model, dataloader, optimizer, criterion, writer, total_batch):
     return loss
 
 
-def train_model(config, model, optimizer, total_batch, loader, num_epochs=10, evaluator=None, val_freq=1000, save_freq=5):
+def train_model(config, model, optimizer, total_batch, train_loader, evalset=None, num_epochs=10,
+                eval_freq=1000,
+                save_freq=5):
     writer = SummaryWriter(config['general']['logDir'])
     writer.add_text('learning rate', f"{config['training']['learning_rate']}")
 
@@ -92,7 +95,7 @@ def train_model(config, model, optimizer, total_batch, loader, num_epochs=10, ev
 
     for epoch in range(config['training']['init_epoch'] + 1, num_epochs):
         print(f'epoch {epoch}:')
-        train_losses = train_epoch_2(model, loader, optimizer, criterion=criterion,
+        train_losses = train_epoch_2(model, train_loader, optimizer, criterion=criterion,
                                      writer=writer, total_batch=total_batch)
 
         epoch_loss = train_losses.mean()
@@ -101,15 +104,8 @@ def train_model(config, model, optimizer, total_batch, loader, num_epochs=10, ev
         writer.add_scalar(f'Epoch/Loss/train', epoch_loss, epoch)
         print('\ntrain loss = {}'.format(epoch_loss))
 
-        if (epoch + 1) % val_freq == 0:
-            model.eval()
-            avg_precision, avg_ndcg = evaluator(model.forward)
-
-            _at = ['@3', '@10', '@100']
-            [writer.add_scalars(f'Epoch/Val/mAP{s}', value) for s, value in zip(_at, avg_precision)]
-            [writer.add_scalars(f'Epoch/Val/NDCG{s}', value) for s, value in zip(_at, avg_ndcg)]
-
-            torch.cuda.empty_cache()
+        if (epoch + 1) % eval_freq == 0:
+            eval_epoch(criterion, evalset, model, writer)
 
         if (epoch + 1) % save_freq == 0:
             torch.save({
@@ -118,6 +114,40 @@ def train_model(config, model, optimizer, total_batch, loader, num_epochs=10, ev
                 'total_batch': total_batch,
             }, f"{model_file_name(epoch, config)}.pth")
     return model.eval()  # return trained model
+
+
+def eval_epoch(criterion, evalset, model, writer):
+    model.eval()
+    at = [3, 10, 100]
+    num_queries = len(evalset)
+    precisions = torch.zeros((len(at), num_queries))
+    ndcgs = torch.zeros((len(at), num_queries))
+    losses = 0.
+    for i in range(num_queries):
+        (query, passage), y = evalset[i]
+
+        torch.cuda.empty_cache()
+
+        query = query.to(map_location, non_blocking=True)
+        passage = passage.to(map_location, non_blocking=True)
+        y = y.to(map_location, non_blocking=True)
+
+        pred = model.forward(query, torch.unsqueeze(passage, 0))
+        loss = criterion(pred, y)
+        losses += loss.detach()
+
+        relevant_idx = torch.where(y[..., torch.argsort(pred)] == 1)[2].reshape(-1) + 1
+
+        precision, ndcg = eval_per_query(relev_rank=relevant_idx.numpy(), at=at)
+        precisions[:, i] = precision
+        ndcgs[:, i] = ndcg
+    avg_precision = precisions.mean(dim=1)
+    avg_ndcg = ndcgs.mean(dim=1)
+    _at = ['@3', '@10', '@100']
+    [writer.add_scalars(f'Epoch/Val/mAP{s}', value) for s, value in zip(_at, avg_precision)]
+    [writer.add_scalars(f'Epoch/Val/NDCG{s}', value) for s, value in zip(_at, avg_ndcg)]
+    writer.add_scalar(f'Epoch/Val/Loss', losses / num_queries)
+    torch.cuda.empty_cache()
 
 
 def init_config():
@@ -169,21 +199,29 @@ if __name__ == "__main__":
 
     config = init_config()
 
-    df = pd.read_parquet(train_raw_df)
+    train_df = pd.read_parquet(train_raw_df)
+    val_df = pd.read_parquet(val_raw_df)
 
-    full_dataset = ValidationDataset(all_dataframe=df,
-                                     return_tensors='tuple', fake_tensor=True,
-                                     val_p_tensors=None, queries_tensors=None,
-                                     passages_per_query=config['training']['passages_per_query'])
+    trainset = CustomDataset(all_dataframe=train_df,
+                             return_tensors='tuple', fake_tensor=True,
+                             queries_tensors=None,
+                             passages_tensors=None,
+                             shuffle_passages=True,
+                             passages_per_query=config['training']['passages_per_query'])
 
-    dataloader = DataLoader(full_dataset,
-                            batch_size=config['training']['batch_size'],
-                            shuffle=True, num_workers=0,
-                            pin_memory=False, collate_fn=collate)
+    evalset = ValidationDataset(all_dataframe=val_df,
+                                return_tensors='tuple', fake_tensor=True,
+                                train_p_tensors=None,
+                                val_p_tensors=None,
+                                queries_tensors=None,
+                                return_ids=False)
+    train_data_loader = DataLoader(trainset,
+                                   batch_size=config['training']['batch_size'],
+                                   shuffle=True, num_workers=0,
+                                   pin_memory=False, collate_fn=collate)
 
     model, optimizer, total_batch = load_model(config)
-    # evaluator = init_evaluator(x_val_handler=lambda x: x.reshape(-1, 2, 300).to(map_location),
-    #                            at=[3, 10, 100])
 
-    train_model(config=config, model=model, optimizer=optimizer, total_batch=total_batch, loader=dataloader,
-                num_epochs=10, evaluator=None)
+    train_model(config=config, model=model, optimizer=optimizer, total_batch=total_batch,
+                train_loader=train_data_loader, evalset=evalset,
+                num_epochs=10)
